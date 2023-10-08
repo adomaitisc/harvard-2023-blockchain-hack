3489597e7465312b3b710f3b501569244ce8a384
adomaitisc <caadomaitis@gmail.com>
2023-10-08 08:33:02 -0400

    removed trips page

--- src/app/(authed)/trips/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../api/_utils/auth-options";
-import { redirect } from "next/navigation";
-
-export default async function NearTrips() {
-  const session = await getServerSession(authOptions);
-  if (!session) return redirect("/");
-}

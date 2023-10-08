9ae2a5a091c0143b2eb12e4c492fe93db8f9d62e
Nicolas Alonso Froeling <nicolasalonsofroeling@Nicolass-MacBook-Pro.local>
2023-10-08 03:03:41 -0400

    post trip

--- src/app/api/trip/route.ts
+++ src/app/api/trip/route.ts
@@ -1,6 +1,5 @@
 import { conn } from "@/_lib/db/client"
-import { NextResponse } from "next/server";
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 
 type requestBody = {
     origin: string,
@@ -13,35 +12,38 @@ type requestBody = {
 
 export async function POST(request: Request) {
     const data: requestBody = await request.json()
-    let trip_id = uuid()
+    let trip_id = uuidv4()
     try {
         const { origin, destination, trip } = data
         if (trip == "Regular") {
             const { time, schedule } = data
-            let sql = "INSERT INTO RegularTrips (trip_id, origin, destination, time, schedule) VALUES (?, ?, ?, ?, ?);"
+            let sql = "INSERT INTO RegularTrip (trip_id, origin, destination, time, schedule) VALUES (?, ?, ?, ?, ?);"
             let query = await conn.execute(sql, [
                 trip_id, origin, destination, time, schedule
             ])
-            return NextResponse.json({ 
+            return new Response(JSON.stringify({ 
                 success: true,
-                trip_id: query.insertId
-            })
+                trip_id: query.insertId,
+                message: "Stored succesfully"
+            }), { status: 200 })
         } else if (trip == "One-time") {
             const { datetime } = data
             let sql = "INSERT INTO OnetimeTrip (trip_id, origin, destination, datetime) VALUES (?, ?, ?, ?);"
             let query = await conn.execute(sql, [
                 trip_id, origin, destination, datetime
             ])
-            return NextResponse.json({
+            return new Response(JSON.stringify({
                 success: true,
-                trip_id: query.insertId
-            })
+                trip_id: query.insertId,
+                message: "Stored succesfully"
+            }), {status: 200})
         }
     }
-    catch {
-        return NextResponse.json({
+    catch (e){
+        console.log(e)
+        return new Response(JSON.stringify({
             success: false,
             message: "An error occurred while processing your request."
-        })
+        }), {status:500})
     }
   }
